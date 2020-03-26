f520992b7082aab5724efa144e7e87c8a56105df
Abinash Sinha <abinashsinha330@gmail.com>
2020-03-26 04:43:47 -0500

    Update plain_rnn_test.py

--- plain_rnn_test.py
+++ plain_rnn_test.py
@@ -11,17 +11,46 @@ from sklearn.metrics import accuracy_score
 print('Loading test data...')
 
 train_meta = pd.read_csv('training_set_metadata.csv')
-test_data = pd.read_csv('test_set.csv')
-
-test_samples = utils.get_data(test_data, train_meta, use_specz=utils.use_specz)
 
 filename = 'model_001.hdf5'
-
-test_x, test_y = utils.get_keras_data(test_samples)
-del test_samples
-
 model = load_model(filename, custom_objects={'mywloss': utils.mywloss})
-preds = model.predict(test_x, batch_size=utils.batch_size2)
-loss = utils.multi_weighted_logloss(test_y, preds, utils.wtable)
-acc = accuracy_score(np.argmax(test_y, axis=1), np.argmax(preds, axis=1))
-print('MW Loss: {0:.4f}, Accuracy: {1:.4f}'.format(loss, acc))
+
+chunk_test_sizes = list()
+chunk_test_mw_losses = list()
+chunk_test_acc = list()
+
+idx = 0
+for chunk_test_data in pd.read_csv('test_set.csv', chunksize=1000000):
+    chunk_test_samples = utils.get_data(chunk_test_data, train_meta, use_specz=utils.use_specz)
+
+    chunk_test_x, chunk_test_y = utils.get_keras_data(chunk_test_samples)
+    del chunk_test_samples
+
+    preds = model.predict(chunk_test_x, batch_size=utils.batch_size2)
+    loss = utils.multi_weighted_logloss(chunk_test_y, preds, utils.wtable)
+    acc = accuracy_score(np.argmax(chunk_test_y, axis=1), np.argmax(preds, axis=1))
+    print('Chunk {} -> MW Loss: {.4f}, Accuracy: {.4f}'.format(idx+1, loss, acc))
+
+    chunk_test_sizes.append(chunk_test_samples.shape[0])
+    chunk_test_mw_losses.append(loss)
+    chunk_test_acc.append(acc)
+
+    idx = idx + 1
+
+print('Total chunks processed before overall test data metric calculation: {}'.format(idx))
+ctr = 0
+test_acc = 0
+test_mw_loss = 0
+for size in chunk_test_sizes:
+    test_acc = test_acc + size * chunk_test_acc[ctr]
+    test_mw_loss = test_mw_loss + size * chunk_test_mw_losses[ctr]
+    ctr = ctr + 1
+
+print('Total chunks processed after overall test data metric calulcation: {}'.format(ctr))
+
+assert idx == ctr
+
+test_size = sum(chunk_test_sizes)
+test_acc = test_acc / test_size
+test_mw_loss = test_mw_loss / test_size
+print('Net Test MW Loss: {.4f}, Accuracy: {.4f}'.format(test_mw_loss, test_acc))
