00ac4762ed944076c31429262898a0bc1825d10f
Abinash Sinha <abinashsinha330@gmail.com>
2020-03-26 03:59:12 -0500

    code updated

--- rnn_model.py
+++ rnn_model.py
@@ -1,3 +1,7 @@
+"""
+Python package to train plain GRU-based RNN model for light curve classification
+"""
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
@@ -20,24 +24,35 @@ from sklearn.metrics import confusion_matrix
 
 import tensorflow as tf
 
-augment_count = 25
-batch_size = 1000
-batch_size2 = 5000
-optimizer = 'nadam'
-num_models = 1
-use_specz = False
-valid_size = 0.1
-max_epochs = 1000
+import plain_rnn_utils as utils
 
-limit = 1000000
-sequence_len = 256
 
-classes = np.array([6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95, 99], dtype='int32')
-class_names = ['class_6','class_15','class_16','class_42','class_52','class_53','class_62','class_64','class_65','class_67','class_88','class_90','class_92','class_95','class_99']
-class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1, 99: 1}
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-# LSST passbands (nm)  u    g    r    i    z    y      
-passbands = np.array([357, 477, 621, 754, 871, 1004], dtype='float32')
+# print(os.listdir("../input"))
+
+# Any results you write to the current directory are saved as output.
+
+
+# augment_count = 25
+# batch_size = 1000
+# batch_size2 = 5000
+# optimizer = 'nadam'
+# num_models = 1
+# use_specz = False
+# valid_size = 0.1
+# max_epochs = 1000
+#
+# limit = 1000000
+# sequence_len = 256
+#
+# classes = np.array([6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95, 99], dtype='int32')
+# class_names = ['class_6','class_15','class_16','class_42','class_52','class_53','class_62','class_64','class_65','class_67','class_88','class_90','class_92','class_95','class_99']
+# class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1, 99: 1}
+#
+# # LSST passbands (nm)  u    g    r    i    z    y
+# passbands = np.array([357, 477, 621, 754, 871, 1004], dtype='float32')
 
 
 def append_data(list_x, list_y = None):
@@ -51,20 +66,6 @@ def append_data(list_x, list_y = None):
         return X
     else:
         return X, np.concatenate(list_y)
-        
-        
-def get_wtable(df):
-    
-    all_y = np.array(df['target'], dtype = 'int32')
-
-    y_count = np.unique(all_y, return_counts=True)[1]
-
-    wtable = np.ones(len(classes))
-
-    for i in range(0, y_count.shape[0]):
-        wtable[i] = y_count[i] / all_y.shape[0]
-
-    return wtable
 
 
 def get_keras_data(itemslist):
@@ -90,15 +91,6 @@ def get_keras_data(itemslist):
 
     return X, Y
 
-
-def set_intervals(sample):
-
-    hist = sample['hist']
-    band = sample['band']
-
-    hist[:,4] = np.ediff1d(hist[:,0], to_begin = [0])
-    hist[:,5] = np.ediff1d(hist[:,0], to_end = [0])
-    
 
 def copy_sample(s, augmentate=True):
     c = copy.deepcopy(s)
@@ -183,116 +175,20 @@ def augmentate(samples, gl_count, exgl_count):
     return res
 
 
-def get_data(data_df, meta_df, extragalactic=None, use_specz=False):
-
-    samples = []
-    groups = data_df.groupby('object_id')
-
-    for g in groups:
-
-        id = g[0]
-
-        sample = {}
-        sample['id'] = int(id)
-
-        #object_id,ra,decl,gal_l,gal_b,ddf,hostgal_specz,hostgal_photoz,hostgal_photoz_err,distmod,mwebv,target
-        #615,349.046051,-61.943836,320.796530,-51.753706,1,0.0000,0.0000,0.0000,nan,0.017,92
-        meta = meta_df.loc[meta_df['object_id'] == id]
-
-        if extragalactic == True and float(meta['hostgal_photoz']) == 0:
-            continue
-
-        if extragalactic == False and float(meta['hostgal_photoz']) > 0:
-            continue
-
-        if 'target' in meta:
-            sample['target'] = np.where(classes == int(meta['target']))[0][0]
-        else:
-            sample['target'] = len(classes) - 1
-
-        sample['meta'] = np.zeros(10, dtype = 'float32')
-
-        sample['meta'][4] = meta['ddf']
-        sample['meta'][5] = meta['hostgal_photoz']
-        sample['meta'][6] = meta['hostgal_photoz_err']
-        sample['meta'][7] = meta['mwebv']
-        sample['meta'][8] = float(meta['hostgal_photoz']) > 0
-
-        sample['specz'] = float(meta['hostgal_specz'])
-
-        if use_specz:
-            sample['meta'][5] = float(meta['hostgal_specz'])
-            sample['meta'][6] = 0.0
-
-        z = float(sample['meta'][5])
-
-        #object_id,mjd,passband,flux,flux_err,detected
-        #615,59750.4229,2,-544.810303,3.622952,1
-
-        mjd      = np.array(g[1]['mjd'],      dtype='float32')
-        band     = np.array(g[1]['passband'], dtype='int32')
-        flux     = np.array(g[1]['flux'],     dtype='float32')
-        flux_err = np.array(g[1]['flux_err'], dtype='float32')
-        detected = np.array(g[1]['detected'], dtype='float32')
-
-        mjd -= mjd[0]
-        mjd /= 100 # Earth time shift in day*100
-        mjd /= (z + 1) # Object time shift in day*100
-
-        received_wavelength = passbands[band] # Earth wavelength in nm
-        received_freq = 300000 / received_wavelength # Earth frequency in THz
-        source_wavelength = received_wavelength / (z + 1) # Object wavelength in nm
-
-        sample['band'] = band + 1
-
-        sample['hist'] = np.zeros((flux.shape[0], 8), dtype='float32')
-        sample['hist'][:,0] = mjd
-        sample['hist'][:,1] = flux
-        sample['hist'][:,2] = flux_err
-        sample['hist'][:,3] = detected
-
-        sample['hist'][:,6] = (source_wavelength/1000)
-        sample['hist'][:,7] = (received_wavelength/1000)
-
-        set_intervals(sample)
-
-        flux_max = np.max(flux)
-        flux_min = np.min(flux)
-        flux_pow = math.log2(flux_max - flux_min)
-        sample['hist'][:,1] /= math.pow(2, flux_pow)
-        sample['hist'][:,2] /= math.pow(2, flux_pow)
-        sample['meta'][9] = flux_pow / 10
-
-        samples.append(sample)
-
-        if len(samples) % 1000 == 0:
-            print('Converting data {0}'.format(len(samples)), end='\r')
-
-        if len(samples) >= limit:
-            break
-
-
-    print()
-    return samples
-        
-
 print('Loading train data...')
 
 train_meta = pd.read_csv('training_set_metadata.csv')
 train_data = pd.read_csv('training_set.csv')
 
 
-wtable = get_wtable(train_meta)
+wtable = utils.get_wtable(train_meta)
 
 
-def mywloss(y_true,y_pred):
-    yc=tf.clip_by_value(y_pred,1e-15,1-1e-15)
-    loss=-(tf.reduce_mean(tf.reduce_mean(y_true*tf.log(yc),axis=0)/wtable))
-    return loss
-    
-    
 def multi_weighted_logloss(y_ohe, y_p, wtable):
-  
+    """
+    @author olivier https://www.kaggle.com/ogrellier
+    multi logloss for PLAsTiCC challenge
+    """
     # Normalize rows and limit y_preds to 1e-15, 1-1e-15
     y_p = np.clip(a=y_p, a_min=1e-15, a_max=1-1e-15)
     # Transform to log
@@ -309,7 +205,7 @@ def multi_weighted_logloss(y_ohe, y_p, wtable):
         nb_pos[-1] = 1
 
     # Weight average and divide by the number of positives
-    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+    class_arr = np.array([utils.class_weight[k] for k in sorted(class_weight.keys())])
     y_w = y_log_ones * class_arr / nb_pos    
     loss = - np.sum(y_w) / np.sum(class_arr)
     return loss / y_ohe.shape[0]
@@ -358,25 +254,25 @@ def train_model(i, samples_train, samples_valid):
 
     if i == 1:
         model.summary()
-    model.compile(optimizer=optimizer, loss=mywloss, metrics=['accuracy'])
+    model.compile(optimizer=utils.optimizer, loss=utils.mywloss, metrics=['accuracy'])
 
-    print('Training model {0} of {1}, Patience: {2}'.format(i, num_models, patience))
+    print('Training model {0} of {1}, Patience: {2}'.format(i, utils.num_models, patience))
     filename = 'model_{0:03d}.hdf5'.format(i)
     callbacks = [EarlyStopping(patience=patience, verbose=1), ModelCheckpoint(filename, save_best_only=True)]
 
-    model.fit(train_x, train_y, validation_data=(valid_x, valid_y), epochs=max_epochs, batch_size=batch_size, callbacks=callbacks, verbose=2)
+    model.fit(train_x, train_y, validation_data=(valid_x, valid_y), epochs=utils.max_epochs, batch_size=utils.batch_size, callbacks=callbacks, verbose=2)
 
-    model = load_model(filename, custom_objects={'mywloss': mywloss})
+    model = load_model(filename, custom_objects={'mywloss': utils.mywloss})
 
-    preds = model.predict(valid_x, batch_size=batch_size2)
+    preds = model.predict(valid_x, batch_size=utils.batch_size2)
     loss = multi_weighted_logloss(valid_y, preds, wtable)
     acc = accuracy_score(np.argmax(valid_y, axis=1), np.argmax(preds,axis=1))
     print('MW Loss: {0:.4f}, Accuracy: {1:.4f}'.format(loss, acc))
 
 
-samples = get_data(train_data, train_meta, use_specz=use_specz)
+samples = utils.get_data(train_data, train_meta, use_specz=utils.use_specz)
 
-for i in range(1, num_models+1):
+for i in range(1, utils.num_models+1):
 
     samples_train, samples_valid = train_test_split(samples, test_size=valid_size, random_state=42*i)
     train_model(i, samples_train, samples_valid)
