cdd7646c3f7157c1d567d42877efe8983d461f7e
Abinash Sinha <abinashsinha330@gmail.com>
2020-04-01 04:47:43 -0500

    Update plain_rnn_test.py

--- plain_rnn_test.py
+++ plain_rnn_test.py
@@ -7,50 +7,53 @@ import pandas as pd
 import numpy as np
 from keras.models import load_model
 from sklearn.metrics import accuracy_score
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
 print('Loading test data...')
 
-train_meta = pd.read_csv('training_set_metadata.csv')
+test_meta = pd.read_csv(utils.test_meta_filepath)
 
-filename = 'model_001.hdf5'
-model = load_model(filename, custom_objects={'mywloss': utils.mywloss})
+wtable = utils.get_wtable(test_meta, is_train=False)
 
-chunk_test_sizes = list()
-chunk_test_mw_losses = list()
-chunk_test_acc = list()
 
-idx = 0
-for chunk_test_data in pd.read_csv('test_set.csv', chunksize=1000000):
-    chunk_test_samples = utils.get_data(chunk_test_data, train_meta, use_specz=utils.use_specz)
+def mywloss(y_true,y_pred):
+    yc=tf.clip_by_value(y_pred,1e-15,1-1e-15)
+    loss=-(tf.reduce_mean(tf.reduce_mean(y_true*tf.log(yc),axis=0)/wtable))
+    return loss
 
-    chunk_test_x, chunk_test_y = utils.get_keras_data(chunk_test_samples)
-    del chunk_test_samples
 
-    preds = model.predict(chunk_test_x, batch_size=utils.batch_size2)
-    loss = utils.multi_weighted_logloss(chunk_test_y, preds, utils.wtable)
-    acc = accuracy_score(np.argmax(chunk_test_y, axis=1), np.argmax(preds, axis=1))
-    print('Chunk {} -> MW Loss: {.4f}, Accuracy: {.4f}'.format(idx+1, loss, acc))
+model = load_model(utils.model_filepath, custom_objects={'mywloss': mywloss})
 
-    chunk_test_sizes.append(chunk_test_samples.shape[0])
-    chunk_test_mw_losses.append(loss)
-    chunk_test_acc.append(acc)
+test_data = pd.read_csv('test_set.csv')
+test_samples = utils.get_data(test_data, test_meta, use_specz=utils.use_specz, is_train_data=False)
 
-    idx = idx + 1
+test_x, test_y = utils.get_keras_data(test_samples)
+del test_samples
 
-print('Total chunks processed before overall test data metric calculation: {}'.format(idx))
-ctr = 0
-test_acc = 0
-test_mw_loss = 0
-for size in chunk_test_sizes:
-    test_acc = test_acc + size * chunk_test_acc[ctr]
-    test_mw_loss = test_mw_loss + size * chunk_test_mw_losses[ctr]
-    ctr = ctr + 1
+test_preds = model.predict(test_x, batch_size=utils.batch_size2)
 
-print('Total chunks processed after overall test data metric calulcation: {}'.format(ctr))
+test_labels = np.argmax(test_preds, axis=1)
+con_mat = tf.confusion_matrix(labels=test_y, predictions=test_labels).numpy()
 
-assert idx == ctr
+con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+con_mat_df = pd.DataFrame(con_mat_norm, index = utils.labels, columns = utils.classes)
 
-test_size = sum(chunk_test_sizes)
-test_acc = test_acc / test_size
-test_mw_loss = test_mw_loss / test_size
-print('Net Test MW Loss: {.4f}, Accuracy: {.4f}'.format(test_mw_loss, test_acc))
+figure = plt.figure(figsize=(8, 8))
+sns.heatmap(con_mat_df, annot=True, cmap='Blues')
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.savefig('test_confusion_matrix.png')
+
+test_loss = utils.multi_weighted_logloss(test_y, test_preds, wtable)
+test_acc = accuracy_score(np.argmax(test_y, axis=1), test_labels)
+print('Test MW Loss: {.4f}, Accuracy: {.4f}'.format(test_loss, test_acc))
+
+true_and_preds_df = pd.DataFrame()
+
+true_and_preds_df['true_label'] = np.argmax(test_y, axis=1).tolist()
+true_and_preds_df['pred_label'] = test_labels.tolist()
+
+true_and_preds_df.to_csv('plain_rnn_true_and_pred.csv', index=False)
